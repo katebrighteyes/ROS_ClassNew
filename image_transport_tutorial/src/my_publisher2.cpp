4bf147836db77374e6e19d96234102ed59b00e86
Kate <49902595+katebrighteyes@users.noreply.github.com>
2021-12-23 21:34:41 +0900

    Update my_publisher2.cpp

--- image_transport_tutorial/src/my_publisher2.cpp
+++ image_transport_tutorial/src/my_publisher2.cpp
@@ -3,6 +3,9 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 
+using namespace cv;
+using namespace std;
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "image_publisher");
@@ -11,11 +14,20 @@ int main(int argc, char** argv)
   image_transport::Publisher pub = it.advertise("camera/image", 1);
 
   //cv::Mat image = cv::imread(argv[1], cv::IMREAD_COLOR);
-  cv::Mat image = cv::imread("/home/opencv-mds/Documents/cat_2.jpg", cv::IMREAD_COLOR);
-  sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
-
-  ros::Rate loop_rate(5);
+  //cv::Mat image = cv::imread("/home/opencv-mds/Documents/cat_2.jpg", cv::IMREAD_COLOR);
+  //sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
+  
+  VideoCapture cap("/home/opencv-mds/Documents/run3.mp4");
+	if (!cap.isOpened())
+	{
+		printf("Can't open the camera");
+		return -1;
+	}
+  ros::Rate loop_rate(1);
   while (nh.ok()) {
+    cap >> image;
+    sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();   
+    
     pub.publish(msg);
     ros::spinOnce();
     loop_rate.sleep();
